5a24a2435cdba26659486e22a413ffa46f9130df
Stefan Geisbacher <dev@sgeisbacher.net>
2026-03-08 09:19:50 +0100

    nvim: set up undo-config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- this is needed for rust to show diagnostics while typing
 vim.diagnostic.config({
   update_in_insert = true,
 })
+
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
